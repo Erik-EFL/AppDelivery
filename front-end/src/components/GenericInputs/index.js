00696e225554cd0ce54f24ffa2b730d7ba792a73
erik-efl <erik.lima.f@outlook.com>
2022-09-21 20:41:56 -0300

    feat(source/front-end/components): :sparkles: Adding a new component
    Create a input text generic

---
+++ front-end/src/components/GenericInputs/index.js
@@ -0,0 +1,27 @@
+import PropTypes from 'prop-types';
+import { useState } from 'react';
+import * as Styles from './styles';
+
+export default function InputGeneric({ name, domId, value, placeholder }) {
+  const [search, setSearch] = useState('');
+
+  return (
+    <label htmlFor={ domId }>
+      {name}
+      <Styles.GenericInputText
+        type="text"
+        id={ domId }
+        value={ value }
+        placeholder={ placeholder }
+        onChange={ (event) => setSearch(event.target.value) }
+      />
+    </label>
+  );
+}
+
+InputGeneric.propTypes = {
+  name: PropTypes.string,
+  domId: PropTypes.string,
+  value: PropTypes.string,
+  placeholder: PropTypes.string,
+}.isRequired;
