780497ca7a26cb3a868191c2bcb2fc88d1e9ff0e
Thiago <bodnarthiago@gmail.com>
2022-09-21 15:18:30 -0300

    feat(back-end): :sparkles: Cria loginServices, arquivo para salvar chave jwt e utils/schemas
    services de login, aquivo para armazenar chave jwt e função para rodar schemas joi

---
+++ back-end/src/Services/login.servives.js
@@ -0,0 +1,37 @@
+const Joi = require('joi');
+const db = require('../database/models');
+const { runSchema } = require('../utils/schemas');
+require('dotenv/config');
+const jwt = require('jsonwebtoken');
+const md5 = require('md5');
+const JWT_SECRET = require('../jwt.evaluation.key');
+
+const loginServices = {
+  validateBody: runSchema(Joi.object({
+    email: Joi.string().email().required(),
+    password: Joi.string().min(6).required(),
+  })),
+
+  createToken(data) {
+    const token = jwt.sign({ data }, JWT_SECRET, {});
+    return token;
+  },
+
+  async login(payload) {
+    const user = await db.Users.findOne({
+      where: { email: payload.email }
+    })
+
+    const passwordHash = md5(payload.password);
+    const { password, ...userWithoutPassword } = user.dataValues;
+
+    if (!user || passwordHash !== password) {
+      throw new Error('Invalid fields');
+    }
+
+    const token = this.createToken(userWithoutPassword);
+
+    return { token };
+  }
+
+}
