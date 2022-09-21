bc422f0f496ad6b9519554c5f294fb7d4e216484
erik-efl <erik.lima.f@outlook.com>
2022-09-20 22:10:39 -0300

    fix(src/backend/ser/database): :bug: Fixing seeders order

--- back-end/src/database/seeders/20220920193504-users.js
+++ back-end/src/database/seeders/20220920193504-users.js
@@ -2,54 +2,33 @@
 
 module.exports = {
   async up (queryInterface, Sequelize) {
-await queryInterface.bulkInsert('Users',
+await queryInterface.bulkInsert('users',
   [
     {
-      id: 200,
-      name: 'John Doe',
-      password: 'Ful4N@Gr1ng@',
-      email: 'JohnDoe@hyamara.com',
-      role: 'customer'
-    },
-    {
-      id: 5,
-      name: 'Gabriel Pesch',
-      password: 'Ful4N@QueMeDevePizza',
-      email: 'GabrielPesch@microlins.com',
-      role: 'seller'
-    },
-    {
       id: 1,
-      name: 'Claudio',
-      password: 'MascoteT19',
-      email: 'Claudio@calud.com',
+      name: 'Delivery App Admin',
+      email: 'adm@deliveryapp.com',
+      password: 'a4c86edecc5aee06eff8fdeda69e0d04',
       role: 'administrator'
     },
     {
-      id: 202,
-      name: 'Heleno',
-      password: 'MascoteT19',
-      email: 'Heleno@calud.com',
-      role: 'customer',
+      id: 2,
+      name: 'Fulana Pereira',
+      email: 'fulana@deliveryapp.com',
+      password: '3c28d2b0881bf46457a853e0b07531c6',
+      role: 'seller'
     },
     {
-      id: 206,
-      name: 'Pricila Bom dia',
-      password: 'MascoteT19',
-      email: 'PricilaBomdia@calud.com',
-      role: 'customer',
+      id: 3,
+      name: 'Cliente Zé Birita',
+      email: 'zebirita@email.com',
+      password: '1c37466c159755ce1fa181bd247cb925',
+      role: 'customer'
     },
-    {
-      id: 190,
-      name: 'Paulo Rocha',
-      password: 'MascoteT19',
-      email: 'PàulinhoRocha@calud.com',
-      role: 'customer',
-    },
-  ], {});
+  ], { timestamps: false });
   },
 
   async down (queryInterface, Sequelize) {
-  await queryInterface.bulkDelete('Users', null, {});
+  await queryInterface.bulkDelete('users', null, {});
   }
 };
