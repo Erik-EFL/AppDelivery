7f70bc3ddac85096d987847f38ff21efff133ff3
erik-efl <erik.lima.f@outlook.com>
2022-09-21 17:24:42 -0300

    feat(front-end): :recycle: Refact: remove desnecessary code

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import './App.css';
-import rockGlass from './images/rockGlass.svg';
 
 function App() {
   return (
     <div className="App">
-      <span className="logo">TRYBE</span>
-      <object className="rocksGlass" type="image/svg+xml" data={ rockGlass }>
-        Glass
-      </object>
+      Ola Mundo
     </div>
   );
 }
