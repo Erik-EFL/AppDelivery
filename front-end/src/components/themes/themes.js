3f0685bf622f296f6fe51a5652b5ba9fc5ac32ac
erik-efl <erik.lima.f@outlook.com>
2022-09-21 15:37:54 -0300

    feat(src/components/themes): :sparkles: Adding a new themes

--- front-end/src/components/themes/themes.js
+++ front-end/src/components/themes/themes.js
@@ -1,8 +1,15 @@
 export default {
-  default: {
+  bgPrimary: {
     darkGreen: '#036B52',
     green: '#2FC18C',
     violet: '#421981',
     blue: '#056CF9',
+    lightBlue: '#00CC9B',
+    lemonGreen: '#66CC00',
+    gold: '#CCB800',
+  },
+  text: {
+    black: '#001813',
+    whit: '#F2FFFC',
   },
 };
