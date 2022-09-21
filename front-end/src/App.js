ef2122e956f08ea598ccd1fe125b258f7345a287
Gabriel Melo <gabrimelo070@gmail.com>
2022-09-21 18:35:30 -0300

    feat: create order status button

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/NavBar';
+import StatusButton from './components/StatusButton';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <StatusButton status="ENTREGUE" />
+      <StatusButton status="PREPARANDO" />
+      <StatusButton status="PENDENTE" />
     </div>
   );
 }
