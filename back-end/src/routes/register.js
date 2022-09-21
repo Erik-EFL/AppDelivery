b26d91373389a6c62d861955b93e499312b75a8b
Thiago Corrêa <thiagocorreacontato@gmail.com>
2022-09-21 16:32:37 -0300

    fix: add missing controller

--- back-end/src/routes/register.js
+++ back-end/src/routes/register.js
@@ -1,9 +1,10 @@
 const { Router } = require('express');
+const registerController = require('../Controller/register');
 
 const { fieldsValidation } = require('../middleware/register');
 
 const router = Router();
 
-router.post('/', fieldsValidation);
+router.post('/', fieldsValidation, registerController);
 
 module.exports = router;
