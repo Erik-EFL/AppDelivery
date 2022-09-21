a8afeb72485cce5c7ed10d2ad2d5e0277e972f27
Thiago Corrêa <thiagocorreacontato@gmail.com>
2022-09-21 16:13:59 -0300

    feat: register - controller file created controlle file

---
+++ back-end/src/Controller/register.js
@@ -0,0 +1,10 @@
+const service = require('../Service/register');
+
+const registerController = {
+  register: async (req, res) => {
+    const storedUser = await service.register(req.body);
+    res.status(201).json(storedUser);
+  },
+}
+
+module.exports = registerController;
