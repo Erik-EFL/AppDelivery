9f89410e906909850b86c4929fb9bc023ca79855
Thiago <bodnarthiago@gmail.com>
2022-09-21 15:36:27 -0300

    feat: adiciona rotas no app

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,7 +1,9 @@
 const express = require('express');
-
+const loginRoutes = require('../Routes/login.routes');
+import 'express-async-errors'
 const app = express();
+app.use(express.json());
 
-app.get('/coffee', (_req, res) => res.status(418).end());
+app.use('/login', loginRoutes);
 
 module.exports = app;
