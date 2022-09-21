e365987faff068e7e1b5ff10caaaeb78e3c9d35c
Thiago Corrêa <thiagocorreacontato@gmail.com>
2022-09-21 15:14:13 -0300

    feat: register - route app file added register route in app file

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,7 +1,14 @@
 const express = require('express');
+require('express-async-errors');
+
+const registerRoute = require('../routes/register');
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/coffee', (_req, res) => res.status(418).end());
+
+app; use('/register', registerRoute);
 
 module.exports = app;
