36b5d0602fcc198153fe1ae0a3a91faeba311030
erik-efl <erik.lima.f@outlook.com>
2022-09-22 16:32:51 -0300

    feat(source/front-end/components): :sparkles: Adding a new component
    Creating Generic product card

--- front-end/src/components/ProductCard/index.js
+++ front-end/src/components/ProductCard/index.js
@@ -1,14 +1,16 @@
 import PropTypes from 'prop-types';
+import Counter from '../Counter/index';
 import * as Styles from './styles';
 
-function GenericProductCard({ img, price, decryption, name }) {
+function GenericProductCard({ img, price, description, name }) {
   return (
     <Styles.ProductCard>
+      <span>{`R$ ${price}`}</span>
       <Styles.BoxImage>
         <img src={ img } alt={ ` imagem de ${name}` } />
       </Styles.BoxImage>
       <Styles.Box>
-        
+        <Counter description={ description } />
       </Styles.Box>
     </Styles.ProductCard>
   );
@@ -18,7 +20,7 @@ GenericProductCard.propTypes = {
   name: PropTypes.string,
   img: PropTypes.string,
   price: PropTypes.string,
-  decryption: PropTypes.string,
+  description: PropTypes.string,
 }.isRequired;
 
 export default GenericProductCard;
