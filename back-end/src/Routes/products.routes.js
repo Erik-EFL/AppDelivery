91f4204a686cb830425d0431fda329f26d788307
Thiago <bodnarthiago@gmail.com>
2022-09-22 16:05:12 -0300

    fix(back-end): :pencil2: Corrige rota /:id
    Muda de post para get

--- back-end/src/Routes/products.routes.js
+++ back-end/src/Routes/products.routes.js
@@ -4,6 +4,6 @@ const productsController = require('../Controllers/products.controller');
 const loginRouter = Router();
 
 loginRouter.get('/', productsController.list);
-loginRouter.post('/:id', productsController.get);
+loginRouter.get('/:id', productsController.get);
 
 module.exports = loginRouter;
