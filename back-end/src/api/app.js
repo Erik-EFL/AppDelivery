bfce1904805db8f905d88c90172c74ec5e57af72
Thiago <bodnarthiago@gmail.com>
2022-09-23 10:37:51 -0300

    feat(App): :sparkles: Adiciona salesRouter na API

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -6,6 +6,7 @@ const loginRoutes = require('../Routes/login.routes');
 const productsRoutes = require('../Routes/products.routes');
 
 const errorMiddleware = require('../Middlewares/error.middleware');
+const salesRouter = require('../Routes/sales.routes');
 
 const app = express();
 app.use(express.json());
@@ -14,6 +15,7 @@ app.get('/coffee', (_req, res) => res.status(418).end());
 app.use('/login', loginRoutes);
 app.use('/register', registerRoutes);
 app.use('/products', productsRoutes);
+app.use('/orders', salesRouter);
 app.use(errorMiddleware);
 
 module.exports = app;
