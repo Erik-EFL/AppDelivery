4bace31adac60d5d9b9ae67cb554eb079f4a9801
Gabriel Melo <gabrimelo070@gmail.com>
2022-09-27 17:41:37 -0300

    create & implementing customer marketplace page

--- front-end/src/components/ProductCard/index.js
+++ front-end/src/components/ProductCard/index.js
@@ -2,15 +2,21 @@ import PropTypes from 'prop-types';
 import Counter from '../Counter/index';
 import * as Styles from './styles';
 
-function GenericProductCard({ img, price, description, name }) {
+function GenericProductCard({ img, price, description, name, domId }) {
   return (
     <Styles.ProductCard>
-      <span>{`R$ ${price}`}</span>
+      <span data-testid={ `customer_products__element-card-price-${domId}` }>
+        {`R$ ${price}`}
+      </span>
       <Styles.BoxImage>
-        <img src={ img } alt={ ` imagem de ${name}` } />
+        <img
+          src={ img }
+          alt={ ` imagem de ${name}` }
+          data-testid={ `customer_products__img-card-bg-image-${domId}` }
+        />
       </Styles.BoxImage>
       <Styles.Box>
-        <Counter description={ description } />
+        <Counter description={ description } domId={ domId } />
       </Styles.Box>
     </Styles.ProductCard>
   );
@@ -20,6 +26,7 @@ GenericProductCard.propTypes = {
   name: PropTypes.string,
   img: PropTypes.string,
   price: PropTypes.string,
+  domId: PropTypes.string,
   description: PropTypes.string,
 }.isRequired;
 
