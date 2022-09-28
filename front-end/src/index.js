da6745f29b5942ef3249a4ad2be33bc30ae7f3bc
erik-efl <erik.lima.f@outlook.com>
2022-09-27 22:59:44 -0300

    feat(front-end): :sparkles: proving store for componentes

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 import App from './App';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import store from './store/store';
+import store from './redux/store/store';
 
 ReactDOM.render(
   <Provider store={ store }>
