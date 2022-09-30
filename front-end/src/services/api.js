2f68a6c4d7288ad5b3e54575a049f4e973151b7e
erik-efl <erik.lima.f@outlook.com>
2022-09-30 20:01:13 -0300

    feat(front-end): :sparkles: Addinf a new api consult

--- front-end/src/services/api.js
+++ front-end/src/services/api.js
@@ -8,8 +8,11 @@ const registerUser = (data) => axios.post(`${baseUrl}/register`, data);
 
 const getAllProducts = () => axios.get(`${baseUrl}/products`);
 
+const orderCreate = () => axios.post(`${baseUrl}/orders`);
+
 export {
   requestLogin,
   registerUser,
   getAllProducts,
+  orderCreate,
 };
