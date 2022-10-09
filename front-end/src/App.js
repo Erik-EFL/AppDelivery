9d6f005d6f3e7b5ab8095b6840a738e347408454
erik-efl <erik.lima.f@outlook.com>
2022-10-09 16:09:13 -0300

    refactor(front-end): :fire: remove code

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import './App.css';
 import AppRoutes from './Routes/Routes';
-// import { GenericContainer } from './components/index';
 
 function App() {
   return (
     <div className="App">
       <AppRoutes />
-      {/* <GenericContainer userRole="customer" /> */}
     </div>
   );
 }
