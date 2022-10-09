fadc4e763b7bcd6548cd3b2f33e33894354f6091
erik-efl <erik.lima.f@outlook.com>
2022-10-09 05:10:39 -0300

    feat(front-end): :sparkles: Adding a new component
    Creating component Order details
    
    this compoente it reponsible for showing mores details of shops

---
+++ front-end/src/pages/OrderDetails/style.js
@@ -0,0 +1,36 @@
+import styled from 'styled-components';
+import themes from '../../components/themes/themes';
+
+export const Container = styled.div`
+  position: relative;
+  height: 100%;
+  width: 100%;
+  display: flex;
+  margin-top: .5rem;
+  flex-direction: column;
+
+  header {
+    width: 80%;
+    margin: 0 auto;
+    padding: 16px;
+    font-size: ${themes.typography.textSize.lg};
+  }
+
+  main {
+    position: relative;
+    width: 80%;
+    margin: 0 auto;
+    margin-bottom: 4rem;
+    height: 77vh;
+    background: ${themes.bgPrimary.white};
+    box-shadow: 0px 4px 4px ${themes.bgPrimary.blackShadow};
+  }
+`;
+
+export const Box = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 90%;
+  flex-direction: ${(props) => (props.direction === 'row' ? 'row' : 'column')};
+  padding: .5rem;
+`;
