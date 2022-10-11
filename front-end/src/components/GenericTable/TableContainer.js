25fbf309c0519748b6a2723e58dbf437a9077ce2
erik-efl <erik.lima.f@outlook.com>
2022-10-11 02:24:40 -0300

    fix(front-end): :bug: Missing semantic elements

--- front-end/src/components/GenericTable/TableContainer.js
+++ front-end/src/components/GenericTable/TableContainer.js
@@ -6,61 +6,53 @@ const renderTable = (pageName, userRole) => {
   if (pageName === 'Detalhe do Pedido' && userRole === 'customer') {
     return (
       <Styles.TableHeader>
-        <tbody>
-          <tr>
-            <th style={ { width: '5rem' } }>Item</th>
-            <th style={ { width: '50rem' } }>Descrição</th>
-            <th style={ { width: '21rem' } }>Quantidade</th>
-            <th style={ { width: '21rem' } }>Valor Unitário</th>
-            <th style={ { width: '21rem' } }>Sub-Total</th>
-          </tr>
-        </tbody>
+        <tr>
+          <th style={ { width: '5rem' } }>Item</th>
+          <th style={ { width: '50rem' } }>Descrição</th>
+          <th style={ { width: '21rem' } }>Quantidade</th>
+          <th style={ { width: '21rem' } }>Valor Unitário</th>
+          <th style={ { width: '21rem' } }>Sub-Total</th>
+        </tr>
       </Styles.TableHeader>
     );
   }
   if (pageName === 'Detalhe do Pedido' && userRole === 'seller') {
     return (
       <Styles.TableHeader>
-        <tbody>
-          <tr>
-            <th style={ { width: '5rem' } }>Item</th>
-            <th style={ { width: '50rem' } }>Descrição</th>
-            <th style={ { width: '21rem' } }>Quantidade</th>
-            <th style={ { width: '21rem' } }>Valor Unitário</th>
-            <th style={ { width: '21rem' } }>Sub-Total</th>
-          </tr>
-        </tbody>
+        <tr>
+          <th style={ { width: '5rem' } }>Item</th>
+          <th style={ { width: '50rem' } }>Descrição</th>
+          <th style={ { width: '21rem' } }>Quantidade</th>
+          <th style={ { width: '21rem' } }>Valor Unitário</th>
+          <th style={ { width: '21rem' } }>Sub-Total</th>
+        </tr>
       </Styles.TableHeader>
     );
   }
   if (pageName === 'Finalizar Pedido' && userRole === 'customer') {
     return (
       <Styles.TableHeader>
-        <tbody>
-          <tr>
-            <th style={ { width: '7rem' } }>Item</th>
-            <th style={ { width: '55rem' } }>Descrição</th>
-            <th style={ { width: '15rem' } }>Quantidade</th>
-            <th style={ { width: '15rem' } }>Valor Unitário</th>
-            <th style={ { width: '15rem' } }>Sub-Total</th>
-            <th style={ { width: '27rem' } }>Remover item</th>
-          </tr>
-        </tbody>
+        <tr>
+          <th style={ { width: '5rem' } }>Item</th>
+          <th style={ { width: '100rem' } }>Descrição</th>
+          <th style={ { width: '28rem' } }>Quantidade</th>
+          <th style={ { width: '20rem' } }>Valor Unitário</th>
+          <th style={ { width: '23rem' } }>Sub-Total</th>
+          <th style={ { width: '45rem' } }>Remover item</th>
+        </tr>
       </Styles.TableHeader>
     );
   }
   if (pageName === 'admin' && userRole === 'administrator') {
     return (
       <Styles.TableHeader>
-        <tbody>
-          <tr>
-            <th style={ { width: '5rem' } }>&nbsp;</th>
-            <th style={ { width: '30rem' } }>Nome</th>
-            <th style={ { width: '30rem' } }>Email</th>
-            <th style={ { width: '30rem' } }>Tipo</th>
-            <th style={ { width: '15rem' } }>Excluir</th>
-          </tr>
-        </tbody>
+        <tr>
+          <th style={ { width: '5rem' } }>&nbsp;</th>
+          <th style={ { width: '30rem' } }>Nome</th>
+          <th style={ { width: '30rem' } }>Email</th>
+          <th style={ { width: '30rem' } }>Tipo</th>
+          <th style={ { width: '15rem' } }>Excluir</th>
+        </tr>
       </Styles.TableHeader>
     );
   }
